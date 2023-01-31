050ab1fe38c206c2f309941355a71765e3b06bc4
Manuel Mockus <mockus.manuel@gmail.com>
2023-01-31 11:26:06 +0100

    Logger: Added logger function and logs

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from models import AEFlow
 from torchmetrics.functional import structural_similarity_index_measure as ssim
-from utils import plotBatch, loss_function
+from utils import plotBatch, loss_function, CustomLogger
+from datetime import datetime
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+#device = "cpu"
 root = '/home/manuel/ae-flow/src/data/chest_xray'
 #root = "./data/chest_xray"
 batch_size = 9
@@ -28,27 +29,30 @@ model = AEFlow('fast_flow', device)
 optimizer = torch.optim.Adam(model.parameters())
 alpha = 0.5
 beta = 0.9
+global_step = 0
+logger = CustomLogger(root + '/runs/' + datetime.now().strftime("%Y%m%d-%H%M%S"))
+
 
 for epoch in range(epochs):
     model.train()
     epoch_loss = 0.0
-    it = 0.0
     for i, (x, _) in enumerate(dataloader):
         if i > 10:
             break
         optimizer.zero_grad()
-        x = x.to(device)
+        x = x.to(device).requires_grad_()
         y, log_prob, logdet_jac = model(x)
-        #x_recon, log_prob, jac = torch.utils.checkpoint.checkpoint(aeflow, x)
+        #y, log_prob, logdet_jac = torch.utils.checkpoint.checkpoint(model, x)
         loss = loss_function(x, y, log_prob.mean(), logdet_jac.mean(), alpha = 1/2,SSIM = False)
         epoch_loss += loss.item()
 
         loss = loss.mean()
         loss.backward()
         optimizer.step()
-        it += 1
-        print(f"Train: epoch {epoch}, iteration: {it} train loss = {loss},")
-    plotBatch(y)
+        global_step += 1
+        anomaly_score = beta * (-torch.exp(log_prob).sum()) + (1 - beta) * ssim(y, x, reduction='sum')
+        print(f"Train: epoch {epoch}, iteration: {global_step} train loss = {loss},")
+        logger.log_all(global_step, loss, anomaly_score, x, y, None)
     if epoch % 2 == 0:
         model.eval()
         test_epoch_loss = 0
@@ -64,6 +68,7 @@ for epoch in range(epochs):
 
             anomaly_score = beta * (-torch.exp(log_prob).sum()) + (1 - beta) * ssim(y, x, reduction='sum')
             epoch_anomaly_score += anomaly_score
+            print(f"Test: epoch {epoch}, iteration: {global_step} train loss = {loss},")
 
         print(f"Test: epoch {epoch}, test loss = {test_epoch_loss/len(test_dataset)},"
               f" score  = {epoch_anomaly_score/len(test_dataset)}")
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,10 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import torch.nn as nn
+import torch
 from torchmetrics.functional import structural_similarity_index_measure as SSIM
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
 
 def loss_function(x,x_prim,log_prob_z_prim,log_det_jac,alpha = 1/2,SSIM = False):
     if SSIM:
@@ -24,3 +27,22 @@ def plotBatch(batch):
         fig.add_subplot(rows, columns, i+1)
         plt.imshow(img)
     plt.show()
+    
+    
+class CustomLogger():
+    def __init__(self, dir: str) -> None:
+        self.logger = SummaryWriter(dir)
+    
+    def log_all(self, step, loss, anomaly, x, x_recon, residual):
+        # TODO: Code Residual
+        self.logger.add_scalar('loss', loss, global_step=step)
+        self.logger.add_scalar('anomaly', anomaly, global_step=step)
+        
+        x_image = x[0:3]
+        x_recon_image = x_recon[0:3]
+        images = torch.cat((x_image, x_recon_image), 0)
+        grid = make_grid(images, nrow = 3, normalize=True, scale_each = True)
+        print(grid.shape)
+        
+        self.logger.add_image('images',grid, step)
+        
